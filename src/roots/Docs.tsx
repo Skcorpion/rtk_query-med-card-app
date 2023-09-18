8b668de13b9a991b726e24f1641ea6d42372492b
Roman Zubko <romanzubko93@gmail.com>
2023-09-18 17:26:06 +0300

    task #11 received docs list in docs

--- src/roots/Docs.tsx
+++ src/roots/Docs.tsx
@@ -1,7 +1,30 @@
+import { useGetUserQuery } from "../services/user";
+
 export default function Docs() {
+  const {isFetching, docs } = useGetUserQuery(
+    undefined,
+    {
+      selectFromResult: ({ data, isFetching }) => ({
+        docs: data?.docs,
+        isFetching,
+      }),
+    }
+  );
+
   return (
     <div>
       <h1>Docs</h1>
+      {isFetching ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <ul>
+            {docs?.map((doc: any) => (
+              <li key={doc.id}>{doc.name}</li>
+            ))}
+          </ul>
+        </>
+      )}
     </div>
   );
 }
